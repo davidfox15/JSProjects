33563c7f95bdae057625fd001c6dd08b123ee1ef
Давид Лисицын <davidfox@MacBook-Air-David.local>
2022-08-25 11:29:07 +0300

    Добавил localStorage

--- fleshcards/main.js
+++ fleshcards/main.js
@@ -1,3 +1,5 @@
+let cardList = [];
+
 const btnClose = document.querySelector("#closeButton");
 const btnCreate = document.querySelector("#createButton");
 const btnDelete = document.querySelector("#allDeleteButton");
@@ -9,6 +11,15 @@ const answer = document.getElementById("answer");
 
 const list = document.querySelector(".ListOfCards");
 
+document.addEventListener("DOMContentLoaded", () => {
+  let json = localStorage.getItem("cards");
+  if (json) {
+    cardList = JSON.parse(json);
+    console.log(cardList);
+    loadList();
+  }
+});
+
 btnClose.addEventListener("click", () => {
   cardForm.classList.add("card_form_hidden");
 });
@@ -18,6 +29,8 @@ btnCreate.addEventListener("click", () => {
 });
 
 btnDelete.addEventListener("click", () => {
+  localStorage.clear();
+  cardList = [];
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
@@ -25,24 +38,42 @@ btnDelete.addEventListener("click", () => {
 
 btnAdd.addEventListener("click", () => {
   if (question.value !== "" && answer.value !== "") {
-    let card = document.createElement("div");
-    card.classList.add("card", "card_item");
-    let cardQuestion = document.createElement("span");
-    cardQuestion.classList.add("card__question");
-    cardQuestion.innerHTML = question.value;
-    let cardAnswer = document.createElement("span");
-    cardAnswer.classList.add("card__answer", "card__answer_hidden");
-    cardAnswer.innerHTML = answer.value;
-    card.appendChild(cardQuestion);
-    card.appendChild(cardAnswer);
-    list.appendChild(card);
+    let obj = { question: question.value, answer: answer.value };
+    console.log(obj);
+    cardList.push(obj);
 
-    card.addEventListener("click", () => {
-      if (cardAnswer.classList.contains("card__answer_hidden")) {
-        cardAnswer.classList.remove("card__answer_hidden");
-      } else {
-        cardAnswer.classList.add("card__answer_hidden");
-      }
-    });
+    localStorage.setItem("cards", JSON.stringify(cardList));
+    console.log(localStorage.getItem("cards"));
+
+    loadList();
   }
 });
+
+const loadList = () => {
+  list.innerHTML = "";
+  for (let i = 0; i < cardList.length; i++) {
+    addCardToList(cardList[i].question,cardList[i].answer);
+  } 
+};
+
+const addCardToList = (question, answer) => {
+  let card = document.createElement("div");
+  card.classList.add("card", "card_item");
+  let cardQuestion = document.createElement("span");
+  cardQuestion.classList.add("card__question");
+  cardQuestion.innerHTML = question;
+  let cardAnswer = document.createElement("span");
+  cardAnswer.classList.add("card__answer", "card__answer_hidden");
+  cardAnswer.innerHTML = answer;
+  card.appendChild(cardQuestion);
+  card.appendChild(cardAnswer);
+  list.appendChild(card);
+
+  card.addEventListener("click", () => {
+    if (cardAnswer.classList.contains("card__answer_hidden")) {
+      cardAnswer.classList.remove("card__answer_hidden");
+    } else {
+      cardAnswer.classList.add("card__answer_hidden");
+    }
+  });
+};
